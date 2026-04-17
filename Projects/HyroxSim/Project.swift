a637346ef26036a92bb75e839cbc1fde4afff5bd
bbdyno <della.kimko@gmail.com>
2026-04-18 00:17:01 +0900

    빌드 2026.04.18.2

--- Projects/HyroxSim/Project.swift
+++ Projects/HyroxSim/Project.swift
@@ -8,7 +8,7 @@
 import ProjectDescription
 
 let appVersion = "1.1.0"
-let appBuildNumber = "2026.04.18.1"
+let appBuildNumber = "2026.04.18.2"
 
 let signingSettings: SettingsDictionary = [
     "SWIFT_VERSION": "5.9",
